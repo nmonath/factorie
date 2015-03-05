517c5ad1b5bd17a95cc088bf02e1e3485d7220da
John Sullivan <john.t.sullivan@gmail.com>
2015-03-05 17:33:02 -0500

    Hier coref implementation compiling.

--- src/main/scala/cc/factorie/app/nlp/hcoref/XDocCorefSystem.scala
+++ src/main/scala/cc/factorie/app/nlp/hcoref/XDocCorefSystem.scala
@@ -1,8 +1,11 @@
 package cc.factorie.app.nlp.hcoref
 
-import cc.factorie.app.nlp.Document
-import cc.factorie.util.{CrossDocEntity, CrossDocEntities}
+import cc.factorie.app.nlp.{CompoundDocumentAnnotator, Document}
+import cc.factorie._
+import cc.factorie.util._
 import cc.factorie.variable.CategoricalDomain
+import cc.factorie.app.nlp.segment.{DeterministicTokenizer, DeterministicSentenceSegmenter}
+import java.io.{BufferedReader, FileReader, File}
 
 /**
  * @author John Sullivan
@@ -10,9 +13,9 @@ import cc.factorie.variable.CategoricalDomain
 // We want to be able to generate XDocMentions from Documents, ?store them? and
 // run coref on the resulting mentions
 trait XDocCorefSystem[T] {
-  def generateEnts(doc:Document):Iterable[(String, Option[String], T)]
-  def getEntId(ent:T):String
-  def doCoref(ments:Seq[T]):Seq[(T, Int)]
+  protected def generateEnts(doc:Document):Iterable[(String, Option[String], T)]
+  protected def getEntId(ent:T):String
+  protected def doCoref(ments:Seq[T]):Seq[(T, Int)]
 
   private val trueEntDomain = new CategoricalDomain[String]()
 
@@ -62,6 +65,17 @@ trait XDocMention[T] {
   def trueEnt:Option[Int]
 }
 
+object XDocMention {
+  def predictedClustering(ments:Iterable[XDocMention[_]]) =
+    new BasicEvaluatableClustering(ments.map(m => m.id -> m.predictedEnt.toString))
+
+  def trueClustering(ments:Iterable[XDocMention[_]]) = if(ments.forall(_.trueEnt.isDefined)) {
+    new BasicEvaluatableClustering(ments.map(m => m.id -> m.trueEnt.get.toString))
+  } else {
+    throw new Exception("All mentions must have a true label defined")
+  }
+}
+
 case class XDocMentionImpl[T](id:String, docPointer:String, withinDocEntityId:String, entity:T) extends XDocMention[T] {
   var predictedEnt = null.asInstanceOf[Int]
   var trueEnt:Option[Int] = None
@@ -80,9 +94,49 @@ class HierCorefSystem[Vars <: NodeVariables[Vars]](getVars:CrossDocEntity => Var
 
   def getEntId(ent: Node[Vars]) = ent.uniqueId
 
-  def generateEnts(doc: Document) = doc.attr[CrossDocEntities].map { docEnt =>
+  def generateEnts(doc: Document) =
+    doc.attr[CrossDocEntities].map { docEnt =>
     val ment = new Mention[Vars](getVars(docEnt))(null)
     docEnt.withinDocEntity foreach (we => ment.withinDocEntityId = we.uniqueId)
     (ment.withinDocEntityId, docEnt.trueLabel, ment)
+  }.toIterable
+}
+
+object JohnSmithExample {
+
+  val getVars = { xEnt:CrossDocEntity =>
+    val vars = new DocEntityVars()
+    vars
+  }
+
+  val getSampler = { ments:Seq[Node[DocEntityVars]] =>
+    val model = new DocEntityCorefModel(4.0, -0.25, 1.0, 2.0, -0.25, 1.0, -0.25, 1.0, -0.25, 1.0, -0.25)
+    implicit val r = new scala.util.Random()
+    new CorefSampler[DocEntityVars](model, ments, ments.size * 100)
+      with AutoStoppingSampler[DocEntityVars]
+      with CanopyPairGenerator[DocEntityVars]
+      with NoSplitMoveGenerator[DocEntityVars] {
+
+      val autoStopThreshold = 10000
+      def newInstance(implicit d:DiffList) = new Node[DocEntityVars](new DocEntityVars)
+    }
+  }
+
+  def main(args:Array[String]) {
+    import FileUtils._
+    val jSmithDir = args(0)
+    val pipeline = new CompoundDocumentAnnotator(Seq(DeterministicTokenizer, DeterministicSentenceSegmenter, new JohnSmithCrossDocAligner(jSmithDir)))
+    val docs =  getRecursiveListOfFiles(jSmithDir).map { f =>
+      new Document(new BufferedReader(new FileReader(f)).toIterator.mkString("\n")).setName(f.getName)
+    }
+    docs foreach pipeline.process
+
+    val system = new HierCorefSystem[DocEntityVars](getVars, getSampler)
+
+    val xDocMentions = docs.flatMap(system.generateXDocMentions)
+
+    val results = system.performCoref(xDocMentions.iterator)
+
+    EvaluatableClustering.evaluationString(XDocMention.predictedClustering(results), XDocMention.trueClustering(results))
   }
 }
